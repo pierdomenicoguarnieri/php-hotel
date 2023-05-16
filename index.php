a1b4b86e416a1f5e7de7c2ce20eb38f1d4fe94df
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-16 18:46:47 +0200

    add: table with foreach cycle

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
           'distance_to_center' => 50
       ],
 
-  ];
+    ];
 
 ?>
 
@@ -48,15 +48,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
     <title>PHP Hotel</title>
   </head>
   <body>
-    <?php foreach ($hotels as $hotel): ?>
-      <ul>
-        <?php foreach ($hotel as $key => $value): ?>
-          <li><?php echo "$key: $value" ?></li>
-        <?php endforeach; ?>
-      </ul>
-    <?php endforeach; ?>
+    <div class="pg-content-wrapper vh-100 bg-dark">
+      <div class="container py-5">
+        <table class="table w-100 rounded-3 overflow-hidden">
+          <thead>
+            
+            <tr class="table-light border-bottom-0">
+
+              <?php foreach ($hotels[0] as $key => $hotel): ?>
+                <th scope="col"><?php echo strtoupper($key); ?></th>
+              <?php endforeach; ?>
+
+            </tr>
+
+          </thead>
+
+          <tbody>
+
+          <?php foreach ($hotels as $hotel): ?>
+
+            <tr class="table-light border-bottom-0">
+
+              <?php foreach ($hotel as $key => $value): ?>
+                <?php if($key === 'parking'){
+                  $value ? $value = '<span class="text-success">Parcheggio disponibile</span>' : $value = '<span class="text-danger">Parcheggio non disponibile</span>';
+                }elseif($key === 'vote' ){
+                  if($value === 3){
+                    $value = "<span class='text-warning'>$value</span>";
+                  }elseif($value <=3){
+                    $value = "<span class='text-danger'>$value</span>";
+                  }else{
+                    $value = "<span class='text-success'>$value</span>";
+                  }
+                };
+                ?>
+
+                <td><?php echo $value ?></td>
+              <?php endforeach; ?>
+
+            </tr>
+
+          <?php endforeach; ?>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
   </body>
 </html>
